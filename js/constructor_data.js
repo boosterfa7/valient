5e4237322d548897a997ea119a353072771ed4b7
boosterfa7 <faflow777@yandex.ru>
2025-05-27 20:04:38 +0300

    Add files via upload

---
+++ js/constructor_data.js
@@ -0,0 +1,17 @@
+const websiteData = {
+  "price_fa7": "250 000",
+  "price_fa7_pro": "350 000",
+  "address": "",
+  "phone": "",
+  "whatsapp": "",
+  "email": "",
+  "website": "",
+  "video_src": "",
+  "hero_image": "",
+  "contact_image": "",
+  "booster_fa7_image": "",
+  "booster_fa7_pro_image": "",
+  "avatar1_image": "",
+  "avatar2_image": "",
+  "avatar3_image": ""
+};
